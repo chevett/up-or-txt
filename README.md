87192979bf6919879091552f47cfecb9eabdefbe
Mike Chevett <mchevett@gmail.com>
2014-07-11 18:06:22 -0400

    up

---
+++ README.md
@@ -0,0 +1,25 @@
+up-or-text
+==========
+
+monitor a url and start txting if it goes down.
+
+This will monitor www.google.com and start txting you if google returns a non 200 more than 3 times in a row.  It will continue to every 10 minutes until it is back up.
+
+
+```js
+var watch = require('up-or-txt');
+
+watch({
+	twilio:{
+		sid: 'your-twilio-sid',
+		token: 'your-twilio-token',
+		number: 'your-twilio-phone-number'
+	},
+	phones: ['your-number'],
+	checkInterval: 1000 * 30,
+	txtInterval: 1000 * 60 * 10,
+	url: 'https://www.google.com/',
+	maxFailureCount: 3,
+	message: 'google is broken!'
+});
+```
---
+++ index.js
@@ -0,0 +1,77 @@
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
+
+var Twilio = require('twilio'),
+	EventEmitter = require('events').EventEmitter,
+	request = require('superagent'),
+	_ = require('lodash');
+
+var DEFAULTS = {
+	twilio: {
+		sid: process.env.TWILIO_ACCOUNT_SID,
+		token: process.env.TWILIO_AUTH_TOKEN,
+	},
+	phones: [],
+	checkInterval: 20 * 1000,
+	txtInterval: 5 * 60 * 1000,
+	maxFailureCount: 3,
+	url: '',
+	message: 'your site is down!'
+};
+
+var failureCount = 0;
+
+function Watcher(params){
+	params = _.extend({}, DEFAULTS, params);
+
+	var emitter = new EventEmitter();
+	var twilio = Twilio(params.twilio.sid, params.twilio.token);
+	var _toFailureTxt = function(number){
+		return _.extend({}, {to: number, from: params.twilio.number, body: params.message});
+	};
+
+	var _sendFailureMessages = function(){
+		if (failureCount === 0) return;
+
+		console.log('sending alerts');
+
+		params.phones
+			.map(_toFailureTxt)
+			.forEach(function(msg){
+				twilio.sms.messages.create(msg, function(error, result) {
+					if (error){
+						console.error(error);
+					} else {
+						console.log('txt sent success.');
+					}
+				});
+		});
+	};
+
+	_sendFailureMessages = _.throttle(_sendFailureMessages, params.txtInterval);
+
+	var _doCheck = function(){
+		request
+			.get(params.url)
+			.end(function(err, res){
+				if (err || !res.body || !res.body.success){
+					failureCount++;
+					console.log('skyjuice is down. ' + failureCount);
+				} else {
+					failureCount = 0;
+					console.log('skyjuice is up.');
+				}
+
+				if (failureCount > params.maxFailureCount){
+					_sendFailureMessages();
+				}
+
+				return _.delay(_doCheck, params.checkInterval);
+			});
+	};
+
+	_doCheck();
+
+	return emitter;
+}
+
+module.exports = Watcher;
